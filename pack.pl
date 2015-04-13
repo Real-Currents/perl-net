1e8ae962b63d6d73dbadcd8afa494760e9e63dd7
John <john@real-currents.com>
2015-04-13 14:05:00 -0700

    add pack

--- pack.pl
+++ pack.pl
@@ -1,6 +1,9 @@
-#!/usr/bin/perl
-use warnings;
+#!/usr/bin/env perl
+use 5.10.1;
 use strict;
+use warnings;
+
+use Socket qw{AF_INET};
 
 my( $ip, $packed );
 $ip = '192.168.2.2';
@@ -21,3 +24,12 @@ print "packed: ". unpack('H*', $packed) ."\n";			# Print hexadecimal representat
 
 ( $a, $b, $c, $d ) = unpack 'C4', $packed;
 print( $ip ."\n") if( $ip eq join '.', $a, $b, $c, $d );
+
+$ip = gethostbyname('localhost');
+print "packed by gethostbyname: ". unpack('H*', $packed) ."\n";	
+my( $name, 
+	$aliases, 
+	$family, 
+	$len, 
+	$packed_addr )= 	gethostbyaddr $ip, AF_INET;
+print "Name: $name, Aliases: $aliases, Net-Family: $family and Length: $len from gethostbyaddr\n";
