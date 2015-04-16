80555b13da00eb242b7bf809994e70cccbea2cf8
Revlin John <stylogicalmaps@gmail.com>
2015-04-16 13:19:21 -0700

    add iplookup

---
+++ iplookup.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use 5.10.1;
+use strict;
+use warnings;
+
+use Socket qw{AF_INET};
+
+while( my $address = shift ) {
+	my $packed_addr = gethostbyname $address;	
+	unless( $packed_addr ) {
+		print "$address => ? \n\t";
+		next;
+	}
+	
+	my( $name, 
+		$aliases, 
+		$family, 
+		$len, 
+		$addr )= gethostbyaddr $packed_addr, AF_INET;
+	print <<DOMAIN_INFO;
+Name: $name 
+Aliases: $aliases 
+Net-Family: $family 
+Length: $len 
+domain info provided by gethostbyaddr
+DOMAIN_INFO
+
+}
